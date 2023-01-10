5e7e1403a5c7558793d9c126adc024daa94060e0
Despot Maksimovic <despotmaksimovic@gmail.com>
2023-01-10 21:46:35 +0100

    improved boilderplate code

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
-import * as dotenv from "dotenv";
-dotenv.config();
-
+import mongoose from "mongoose";
+import cors from "cors";
 import express from "express";
 
 const app = express();
 
+app.use(cors({
+    origin: "http://localhost:3000"
+}));
+
+
+mongoose.connect(process.env.CONN_STRING, () => {
+    console.log(`Connection to MongoDB at ${process.env.CONN_STRING} has been opened successfully`);
+});
 app.listen(process.env.PORT, () => {
     console.log(`Server listening on http://localhost:${process.env.PORT}/`);
 });
