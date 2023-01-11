b31f68a652b55376bffd37b84af8da90fba39612
Despot Maksimovic <despotmaksimovic@gmail.com>
2023-01-11 21:47:51 +0100

    created first versions of header and footer for the frontend

--- client/src/components/Layout/Footer/Footer.jsx
+++ client/src/components/Layout/Footer/Footer.jsx
@@ -1,23 +1,42 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import { FiInstagram, FiFacebook, FiTwitter, FiLinkedin } from "react-icons/fi";
 import "./Footer.css";
 
 const Footer = () => {
   return (
     <footer>
-        <div className="col">
-            <h3>First column</h3>
-            <p>Lorem ipsum dolor sit amet.</p>
+      <div className="tint"></div>
+      <div className="wrapper">
+        <div className="row">
+          <div className="col">
+              <h3>Find your dream home</h3>
+              <Link className='link' to="/">Apartments</Link>
+              <Link className='link' to="/">Condos</Link>
+              <Link className='link' to="/">Houses</Link>
+              <Link className='link' to="/">Offices</Link>
+          </div>
+          <div className="line"></div>
+          <div className="col">
+              <h3>Social media</h3>
+              <a href="/" className="link"><FiInstagram />Instagram</a>
+              <a href="/" className="link"><FiFacebook />Facebook</a>
+              <a href="/" className="link"><FiLinkedin />LinkedIn</a>
+              <a href="/" className="link"><FiTwitter />Twitter</a>
+          </div>
+          <div className="line"></div>
+          <div className="col">
+              <h3>Account management</h3>
+              <Link className="link" to="/login">Log in</Link>
+              <Link className="link" to="/register">Register</Link>
+              <Link className="link" to="/list">List property</Link>
+              <Link className="link" to="/my-profile">View your profile</Link>
+          </div>
         </div>
-        <div className="line"></div>
-        <div className="col">
-            <h3>Second column</h3>
-            <p>Lorem ipsum dolor sit amet.</p>
+        <div className="row">
+          <h3 className='copyright'>&copy; 2023 Despot Maksimovic</h3>
         </div>
-        <div className="line"></div>
-        <div className="col">
-            <h3>Third column</h3>
-            <p>Lorem ipsum dolor sit amet.</p>
-        </div>
+      </div>
     </footer>
   );
 }
--- client/src/components/Layout/Navbar/Navbar.jsx
+++ client/src/components/Layout/Navbar/Navbar.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import { RiAccountCircleFill } from "react-icons/ri";
 import "./Navbar.css";
+import logo from "../../../assets/logo.png";
 
 const Navbar = () => {
   return (
     <nav>
-        <ul>
-            <li>
-                <Link className='link' to="/">Home</Link>
-            </li>
-        </ul>
+      <section>
+        <Link className='link' to="/rent">Rent</Link>
+        <Link className='link' to="/buy">Buy</Link>
+        <Link className='link' to="/post">Post</Link>
+      </section>
+      <section>
+        <Link className='link' to="/">
+          <img src={logo} alt="logo" className='logo' />
+        </Link>
+      </section>
+      <section className='account-area'>
+        <Link className='link' to="/my-profile">
+          <RiAccountCircleFill />
+          My profile
+        </Link>
+      </section>
     </nav>
   );
 }
